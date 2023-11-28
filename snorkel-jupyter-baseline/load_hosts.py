48b8d18fdce8ec6518d1c6fc7b8d1015d9c3b5ee
Maik Fröbe <mam10eks@users.noreply.github.com>
2023-11-28 19:02:06 +0000

    mf

--- snorkel-jupyter-baseline/load_hosts.py
+++ snorkel-jupyter-baseline/load_hosts.py
@@ -1,10 +1,10 @@
 import os
 from utils import ABSTAIN, BENIGN, MALICIOUS, ADULT
-
+from snorkel.labeling import labeling_function
 
 # dir = r'hosts_data/'
 categories = {
-         MALICIOUS: [
+         MALICIOUS: set([
             'add.Risk',
             'add.Spam',
             'hostsVN',
@@ -23,24 +23,28 @@ categories = {
             'StevenBlack',
             'UncheckyAds',
             'URLHaus'
-         ],
-         ADULT:[
+         ]),
+         ADULT: set([
             'someonewhocares.org',
             'StevenBlack'
-         ]
+         ])
 }  
 
 
 def list_files(dir):
     # r = []
     dict_hosts = {}
+    print(dir)
     for root, dirs, files in os.walk(dir):
         for name in files:
             if name == 'hosts':
                 subdir = str(root).split('/')[-1]
                 path = os.path.join(root, name)
                 # r.append(path)
-                dict_hosts = dict_hosts | parse_hosts(path, subdir)
+                for k, v in parse_hosts(path, subdir).items():
+                    if k not in dict_hosts:
+                        dict_hosts[k] = []
+                    dict_hosts[k] += v
     return dict_hosts
 
 
@@ -51,10 +55,35 @@ def parse_hosts(hosts_file_path, subdir):
                 if line.startswith('0.0.0.0 '):
                       host = line.strip()
                       host = host.split(' ')[1]
-                      dict_hosts[host] = subdir
+                      if host not in dict_hosts:
+                        dict_hosts[host] = []
+                      dict_hosts[host] += [subdir]
     return dict_hosts
 
+hosts_file_paths = list_files(os.path.dirname(os.path.realpath(__file__)) + '/hosts_data/')
 
-dict = hosts_file_paths = list_files('snorkel-jupyter-baseline/hosts_data/')
-#for hosts in hosts_file_paths:
- #   parse_hosts(hosts)
+@labeling_function()
+def is_malicious(doc):
+    from urllib.parse import urlparse
+    url = doc['url']
+    host = urlparse(url).netloc
+    if host not in hosts_file_paths:
+        return ABSTAIN
+    
+    for directory in hosts_file_paths[host]:
+        if directory in categories[MALICIOUS]:
+            return MALICIOUS
+    return ABSTAIN
+
+@labeling_function()
+def is_adult(doc):
+    from urllib.parse import urlparse
+    url = doc['url']
+    host = urlparse(url).netloc
+    if host not in hosts_file_paths:
+        return ABSTAIN
+    
+    for directory in hosts_file_paths[host]:
+        if directory in categories[ADULT]:
+            return ADULT
+    return ABSTAIN
